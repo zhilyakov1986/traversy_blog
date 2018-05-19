1ac34af2703ea799d5c24a0512c3e126322152e4
superwer <zhilyakov1986@gmail.com>
2018-05-19 16:22:42 -0400

    first

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users: User[];
+  user: User = {
+    firstName: '',
+    lastName: '',
+    email: ''
+
+  };
+  showExtended = true;
+  loadingUsers = false;
+  enableAdd = false;
+  showUserForm = false;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@gmail.com',
+        isActive: true,
+        registered: new Date('01/02/2018 08:00:00'),
+        hide: true
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        email: 'kevin@gmail.com',
+        isActive: false,
+        registered: new Date('03/02/2017 12:00:00'),
+        hide: true
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        email: 'karen@gmail.com',
+        isActive: true,
+        registered: new Date('08/02/2016 03:00:00'),
+        hide: true
+      }
+    ];
+    this.loadingUsers = true;
+
+  }
+
+  // addUser() {
+  //   this.user.isActive = true;
+  //   this.user.registered = new Date();
+  //   this.users.unshift(this.user);
+  //   this.user = {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: ''
+  //   };
+  // }
+  onSubmit(e) {
+    console.log('submit');
+    e.preventDefault();
+  }
+}
