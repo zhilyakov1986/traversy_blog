e4dd04bc8a9e90c1aa407d6ab54b7c54522765ad
superwer <zhilyakov1986@gmail.com>
2018-05-20 10:57:46 -0400

    template driven validation

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,6 +9,7 @@ export class DataService {
             {
                 firstName: 'John',
                 lastName: 'Doe',
+                email: 'john@gmail.com',
                 isActive: true,
                 registered: new Date('01/02/2018 08:00:00'),
                 hide: true
@@ -16,6 +17,7 @@ export class DataService {
             {
                 firstName: 'Kevin',
                 lastName: 'Johnson',
+                email: 'kevin@gmail.com',
                 isActive: false,
                 registered: new Date('03/02/2017 12:00:00'),
                 hide: true
@@ -23,6 +25,7 @@ export class DataService {
             {
                 firstName: 'Karen',
                 lastName: 'Williams',
+                email: 'karen@gmail.com',
                 isActive: true,
                 registered: new Date('08/02/2016 03:00:00'),
                 hide: true
