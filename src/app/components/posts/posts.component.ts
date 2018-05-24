e03ce1c8a57a559c39e193d81bf822db3a895283
zhilyakov1986 <zhilyakov1986@gmail.com>
2018-05-23 21:48:19 -0400

    delete is ready

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -43,4 +43,15 @@ export class PostsComponent implements OnInit {
       }
     });
   }
+  removePost(post: Post) {
+    if (confirm('Are You Sure?')) {
+      this.postService.removePost(post.id).subscribe(() => {
+        this.posts.forEach((cur, index) => {
+          if (post.id === cur.id) {
+            this.posts.splice(index, 1);
+          }
+        });
+      });
+    }
+  }
 }
